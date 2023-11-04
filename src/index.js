2bc4738b851fcc516d6cc4c481e2954a92e96ca0
Abhishek Kumar <krabhi1729@gmail.com>
2023-11-04 13:10:13 +0530

    Jsx, Element vs Component , Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const isMorning = true;
+const morningText = "Good Morning";
+const afternoonText = "Good Afternoon";
+const handleClick=(state)=> {
+ return console.log("Clicked",state);
+}
 
-const child1=React.createElement('p',{className:'text'},"hey buddy");
-const div = React.createElement('div', {className:'container'}, [child1]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(div);
+const morningElement = (
+  <>
+    {morningText}
+    <span onClick={()=>handleClick(morningText)}>Its 8 am</span>
+  </>
+);
+const afternoonElement = (
+  <>
+    {afternoonText}
+    <span >Its 8 pm</span>
+  </>
+);
+const div = <div>{isMorning ? morningElement : afternoonElement}</div>;
+// console.log(div);
+function GreetingComponent() {
+  return <div>{isMorning ? morningElement : afternoonElement}</div>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<GreetingComponent />);
