3c59da7a0fd4145dfb269217fa1c01c5c1ecc474
Abhishek Kumar <krabhi1729@gmail.com>
2023-11-07 10:33:30 +0530

    render data from the list and importance of keys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Button from "./Button";
+import Text from "./Text"
 
 const App = () => {
+
+const data=[
+  {id:'a' , text:"text 1"},
+  {id:'b' , text:"text 2"},
+  {id:'c' , text:"text 3"},
+  {id:'d' , text:"text 4"},
+  {id:'e' , text:"text 5"},
+]
+
   const handleClickAction=()=>{
     console.log("I was clicked")
   }
   return (
-    <Button clickAction={handleClickAction}>Click Me</Button>
-    // <Button children="Click Me" />
+    <>
+    {data.map((item)=><Text key={item.id}>{item.text}</Text>)}
+    </>
+    // <Button data={{a:{b:{c:'d'}}}} clickAction={handleClickAction}>Click Me</Button>
+    // // <Button children="Click Me" />
   );
 };
 
--- src/Button.js
+++ src/Button.js
@@ -2,13 +2,14 @@
  
  const Button = (props) => {
       console.log(props)
-      const {children , clickAction}=props;
+      const {children , clickAction ,data}=props;
       const handleClick=()=>{
    clickAction()
 }
    return (
 
-     <button onClick={handleClick}>{children}</button>
+     <button onClick={handleClick}>{children} 
+     {data?.a?.b?.c}</button>
    )
  }
  
